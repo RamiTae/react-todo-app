36b5324c0c41f1498fcfb6380111fead84ab0963
RamiTae <ramiTae.dev@gmail.com>
2019-12-18 20:44:29 +0900

    Revert ":rotating_light: checkbox태그의 value에 바로 접근해서 발생하는 warning 해결"
    This reverts commit 642872db497dced08308a12f9c9c2913af948a6a.

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,17 +3,15 @@ import React from "react";
 export default function Todo({ todo, handleTodo, isSearching }) {
   const parseTodo = JSON.parse(todo);
   // console.log(parseTodo);
-  function handleCheckbox(e) {
+  function handleRadio(e) {
     // console.log(handleTodo(parseTodo.index, "completed", !parseTodo.completed));
     handleTodo(parseTodo.index, "completed", !parseTodo.completed);
   }
 
-  const label = parseTodo.completed ? true : false;
-
   return (
     <div>
       {isSearching ? `[${parseTodo.title}] ` : null}
-      <input type="checkbox" onClick={handleCheckbox} defaultChecked={label} />
+      <input type="radio" onClick={handleRadio} checked={parseTodo.completed} />
       <span>{parseTodo.text}</span>
       {/* <button>delete</button> */}
     </div>
