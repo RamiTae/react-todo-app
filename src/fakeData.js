b50b2b94220c859519f8c88f6d5b467f590994a0
RamiTae <ramiTae.dev@gmail.com>
2019-12-22 20:41:22 +0900

    data 수정

--- src/fakeData.js
+++ src/fakeData.js
@@ -1,21 +1,21 @@
 export default function data() {
   return {
-    titles: ["191219", "191220"],
+    titles: ["오늘", "내일"],
     todos: [
       {
-        title: "191219",
+        title: "오늘",
         text: "운동하기",
         index: 0,
         completed: false
       },
       {
-        title: "191219",
+        title: "오늘",
         text: "냉장고 파먹기\n- 토마토\n- 계란",
         index: 1,
         completed: false
       },
       {
-        title: "191220",
+        title: "내일",
         text: "일찍 자기",
         index: 2,
         completed: false
